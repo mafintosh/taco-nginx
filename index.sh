695bd6c8311acc26670ffe105fe4aecfa424da3f
Mathias Buus <mathiasbuus@gmail.com>
2015-05-01 00:57:38 +0200

    allow duplex http

--- index.sh
+++ index.sh
@@ -28,7 +28,7 @@ while true; do
 done
 
 if $VERSION; then
-  printf "1.3.0 \n"
+  printf "1.4.0 \n"
   exit
 fi
 
@@ -82,6 +82,8 @@ server {
     proxy_pass http://$SERVICE_NAME;
     proxy_set_header X-Forwarded-For \$remote_addr;
     proxy_buffering off;
+    proxy_request_buffering: off;
+    proxy_http_version 1.1;
     $LOCATION_CONFIG_CONTENTS
   }
   $SERVER_CONFIG_CONTENTS
